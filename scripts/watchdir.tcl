2c94b2a1d0aa08b926bbf992c7d1f4a0a0e0a251
Cyan Ogilvie <cyan@tirion.(none)>
2010-04-21 10:02:23 +0200

    Added inotify::watchdir class, for simplified directory monitoring

---
+++ scripts/watchdir.tcl
@@ -0,0 +1,112 @@
+# vim: ft=tcl foldmethod=marker foldmarker=<<<,>>> ts=4 shiftwidth=4
+
+oo::class create inotify::watchdir {
+	variable {*}{
+		queue
+		files
+	}
+
+	constructor {dir} { #<<<
+		set files	[dict create]
+		set queue	[inotify::queue new [namespace code {my _fs_event}]]
+		my _setup_watches $dir
+	}
+
+	#>>>
+	destructor { #<<<
+		if {[info exists queue] && [info object isa object $queue]} {
+			$queue destroy
+			unset queue
+		}
+	}
+
+	#>>>
+	method _fs_event {ev} { #<<<
+		?? {
+			puts "Event in watched dir:"
+			array set event $ev
+			parray event
+		}
+		set fqpath	[file join [dict get $ev path] [dict get $ev name]]
+		set mask	[dict get $ev mask]
+		if {"IN_ISDIR" in $mask} {
+			if {"IN_CREATE" in $mask || "IN_MOVED_TO" in $mask} {
+				my _add_watches $fqpath
+			} elseif {"IN_MOVE_SELF" in $mask} {
+				my _deep_remove $fqpath
+			}
+		} else {
+			if {"IN_CREATE" in $mask || "IN_MOVED_TO" in $mask || "IN_CLOSE_WRITE" in $mask} {
+				my _new_file $fqpath
+			} elseif {"IN_DELETE" in $mask || "IN_MOVED_FROM" in $mask} {
+				my _remove_file $fqpath
+			}
+		}
+	}
+
+	#>>>
+	method _setup_watches {dir} { #<<<
+		my _add_watches $dir
+	}
+
+	#>>>
+	method _add_watches {dir} { #<<<
+		$queue add_watch $dir {
+			IN_CLOSE_WRITE
+			IN_CREATE
+			IN_DELETE
+			IN_DELETE_SELF
+			IN_MOVE_SELF
+			IN_MOVED_FROM
+			IN_MOVED_TO
+		}
+		foreach subdir [glob -nocomplain -type d [file join $dir *]] {
+			my _add_watches $subdir
+		}
+		foreach file [glob -nocomplain -type f [file join $dir *]] {
+			my _new_file $file
+		}
+	}
+
+	#>>>
+	method _new_file {fqpath} { #<<<
+		if {![file readable $fqpath]} {
+			puts stderr "New file is not readable: \"$fqpath\""
+			return
+		}
+		if {[file size $fqpath] > 1048576} {
+			puts stderr "New file is bigger than the 1 MiB threshold, ignoring: \"$fqpath\""
+			return
+		}
+		dict set files $fqpath 1
+		my new_file $fqpath
+	}
+
+	#>>>
+	method new_file {fqpath} { #<<<
+	}
+
+	#>>>
+	method _remove_file {fqpath} { #<<<
+		if {[dict exists $files $fqpath]} {
+			dict unset files $fqpath
+			?? {puts "Forgot file \"$fqpath\""}
+			my remove_file $fqpath
+		}
+	}
+
+	#>>>
+	method remove_file {fqpath} { #<<<
+	}
+
+	#>>>
+	method _deep_remove {fqpath} { #<<<
+		foreach key [dict keys $files [file join $fqpath *]] {
+			my _remove_file $fqpath
+		}
+	}
+
+	#>>>
+}
+
+
